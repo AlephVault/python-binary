17213609330dea798fc70046e40efc820c733dcf
Luis Masuelli <luis@datagran.io>
2022-05-06 10:47:52 -0500

    Adding stream compatibility.

--- alephvault/binary/buffers.py
+++ alephvault/binary/buffers.py
@@ -1,7 +1,8 @@
+from typing import Iterable, Union
+from warnings import warn
 from io import SEEK_CUR, SEEK_SET, SEEK_END
 
 _INITIAL_CAPACITY = 16
-_GROWTH_FACTOR = 2.0
 
 
 class UnsupportedError(Exception):
@@ -12,10 +13,7 @@ class UnsupportedError(Exception):
 
 class Buffer:
 
-    def __init__(self, initial_capacity: int = _INITIAL_CAPACITY, growth_factor: float = _GROWTH_FACTOR,
-                 target: bytearray = None):
-        # Initial buffer vars.
-        self.growth_factor = growth_factor
+    def __init__(self, initial_capacity: int = _INITIAL_CAPACITY, target: bytearray = None):
         # Length & position vars.
         self.bit_position = 0
         self._bit_length = 0
@@ -32,19 +30,19 @@ class Buffer:
             self._resizable = True
 
     @property
-    def growth_factor(self):
-        return self._growth_factor
-
-    @growth_factor.setter
-    def growth_factor(self, value: float):
-        self._growth_factor = max(1.0, value)
-
-    @property
     def resizable(self):
+        """
+        Whether the stream is resizable or not.
+        """
+
         return self._resizable
 
     @property
     def length(self):
+        """
+        The current length, in bytes (rounded up).
+        """
+
         return self._bit_length >> 3 + (1 if self._bit_length & 7 else 0)
 
     @length.setter
@@ -57,33 +55,60 @@ class Buffer:
         self.bit_position = min(value << 3, self.bit_position)
 
     def __len__(self):
+        """
+        The current length, in bytes (rounded up).
+        """
+
         return self.length
 
     @property
     def position(self):
+        """
+        The current position, in bytes (rounded down).
+        """
+
         return self.bit_position >> 3
 
     @position.setter
     def position(self, value: int):
+        """
+        Sets the current position, in bytes.
+        :param value: The position, in bytes, to set.
+        """
+
         self.bit_position = value << 3
 
     @property
     def bit_length(self):
+        """
+        The current length of the contents, in bits.
+        """
+
         return self._bit_length
 
     @property
     def bit_aligned(self):
+        """
+        Whether the current length of the contents is in multiples of 8.
+        """
+
         return self.bit_position & 7 == 0
 
     @property
     def capacity(self):
-        return len(self._target)
+        """
+        Gets the underlying buffer capacity.
+        """
 
-    def target(self):
-        return self._target
+        return len(self._target)
 
     @capacity.setter
     def capacity(self, value: int):
+        """
+        Sets the underlying buffer capacity.
+        :param value: The new capacity.
+        """
+
         if value < self.length:
             raise ValueError("New capacity too small!")
         self._set_capacity(value)
@@ -106,6 +131,251 @@ class Buffer:
             new_capacity = max(max(value, 256), self.capacity * 2)
         self._set_capacity(new_capacity)
 
+    @property
+    def target(self):
+        """
+        The underlying buffer.
+        """
+
+        return self._target
+
+    # Compatibility with the binary stream protocol:
+
+    def seek(self, offset: int, whence: int = SEEK_SET):
+        """
+        Moves the pointer to the appropriate position in the buffer.
+        This operation can be a bit dangerous for this stream type
+        if used inappropriately.
+
+        >> offset * 8 (clamped) is used for SEEK_SET.
+        >> (capacity - offset) * 8 (clamped) is used for SEEK_END.
+        >> (bit_position + offset * 8) (clamped) is used for SEEK_CUR.
+
+        :param offset: The offset to seek.
+        :param whence: The reference point.
+        """
+
+        def _clamp(v):
+            return max(0, min(self.capacity << 3, v))
+
+        if whence == SEEK_SET:
+            self.bit_position = _clamp(offset << 3)
+        elif whence == SEEK_CUR:
+            self.bit_position = _clamp(self.bit_position + offset << 3)
+        elif whence == SEEK_END:
+            self.bit_position = _clamp((self.capacity - offset) << 3)
+        else:
+            raise ValueError(f"Invalid seek origin: {whence}")
+
+    def tell(self):
+        """
+        The position to tell is byte-wise.
+        :return:
+        """
+
+        warn("The tell() method may lack of precision for this bit-wise stream, "
+             "in particular when the stream is misaligned")
+        return self.position
+
+    def close(self):
+        """
+        Nothing to do here.
+        """
+
+    def seekable(self):
+        """
+        This stream is seekable, despite it being dangerous.
+        :return: True
+        """
+
+        return True
+
+    def readable(self):
+        """
+        This stream is readable.
+        :return: True
+        """
+
+        return True
+
+    def writable(self):
+        """
+        This stream is writable.
+        :return: True
+        """
+
+        return True
+
+    def closed(self):
+        """
+        This stream is never closed.
+        :return: False
+        """
+
+        return False
+
+    def truncate(self, l: int = 0):
+        """
+        This stream cannot be truncated.
+        :param l: The length to truncate to.
+        :return:
+        """
+
+        raise UnsupportedError("This stream cannot be truncated")
+
+    def readline(self, size: int = -1, /):
+        """
+        This stream is not line-wise. Pure binary.
+        """
+
+        raise UnsupportedError("This stream cannot read lines")
+
+    def readlines(self, size: int = -1, /):
+        """
+        This stream is not line-wise. Pure binary.
+        """
+
+        raise UnsupportedError("This stream cannot read lines")
+
+    def readinto(self, into: bytearray, /, offset: int = 0, size: int = -1):
+        """
+        Reads data into a given bytearray.
+        :param into: The array to read into.
+        :param offset: The offset to start, inside the array.
+        :param size: The size to read. A negative value reads all.
+        :return: The number of bytes read.
+        """
+
+        max_length = self.capacity - self.position - 1 + int(self.bit_aligned)
+        if size < 0:
+            size = len(into)
+        len_ = min(size, max_length)
+        for idx in range(len_):
+            into[offset + idx] = self._read_byte()
+        return len_
+
+    readinto1 = readinto
+
+    def read(self, size: int = -1):
+        """
+        Reads data into a new byte array, up to certain length.
+        :param size: The size to read. A negative value reads all.
+        :return: The bytes read (the array).
+        """
+
+        arr = bytearray(self.length)
+        len_ = self.readinto(arr, offset=0, size=size)
+        return arr[:len_]
+
+    read1 = read
+
+    def writelines(self, lines: Iterable[str]):
+        """
+        This stream is not line-wise. Pure binary.
+        :param lines: The lines to write.
+        """
+
+        raise UnsupportedError("This stream cannot write lines")
+
+    def write(self, b: Union[bytearray, bytes], offset: int = 0, size: int = -1):
+        """
+        Writes bytes from a given source.
+        :param b: The bytes source.
+        :param offset: The offset to write.
+        :param size: The size to write. A negative value reads all.
+        """
+
+        position = self.position
+        len_ = len(b)
+        if size < 0:
+            size = len_
+        elif size > len_:
+            size = len_
+
+        if self.bit_aligned:
+            if position + size >= self.capacity:
+                self._grow(size)
+            self._target[position:position + size] = b[offset:offset + size]
+        else:
+            if position + size + 1 >= self.capacity:
+                self._grow(size)
+            for idx in range(size):
+                self._write_misaligned(b[offset + idx])
+
+        return size
+
+    def getbuffer(self):
+        """
+        Gets the underlying buffer.
+        """
+
+        return self.target
+
+    def getvalue(self):
+        """
+        Gets the contents of the underlying buffer.
+        :return: The contents
+        """
+
+        return bytes(self.target)
+
+    # Extra read & write methods:
+
+    def read_byte(self):
+        """
+        Reads a single byte. On EOF, returns -1.
+        :return: A byte, or -1 on EOF.
+        """
+
+        return self._read_byte() if self._has_data_to_read() else -1
+
+    def read_bit(self):
+        """
+        Reads a single bit. On EOF, returns None
+        :return: The bit (as bool) or None.
+        """
+
+        if self.bit_position >= self.bit_length:
+            return None
+        b = self._target[self.position]
+        self.bit_position += 1
+        return b & (1 << (self.bit_position & 7)) != 0
+
+    def write_byte(self, value: int):
+        """
+        Writes a single byte.
+        :param value: The byte to write.
+        """
+
+        if value < 0 or value > 255:
+            raise ValueError("Value to write must be in range(0, 256)")
+        if self.bit_aligned:
+            if self.position + 1 >= self.capacity:
+                self._grow(1)
+            self._target[self.position] = value
+            self.position += 1
+        else:
+            if self.position + 2 >= self.capacity:
+                self._grow(1)
+            self._write_misaligned(value)
+        self._update_length()
+
+    def write_bit(self, bit: bool):
+        """
+        Writes a single bit.
+        :param bit: The bit to write.
+        """
+
+        p = self.position
+        if self.bit_aligned and self.position == self.capacity:
+            self._grow(1)
+        r = self.bit_position & 7
+        self._target[p] = (self._target[p] & ~(1 << r)) | (int(bit) << r)
+        self.bit_position += 1
+        self._update_length()
+
+    # Private methods:
+
     def _has_data_to_read(self):
         return self.position < self.length
 
@@ -124,59 +394,6 @@ class Buffer:
     def _read_byte(self):
         return self._read_byte_aligned() if self.bit_aligned else self._read_byte_misaligned()
 
-    def read_byte(self):
-        return self._read_byte() if self._has_data_to_read() else -1
-
-    def read_bit(self):
-        b = self._target[self.position]
-        self.bit_position += 1
-        return b & (1 << (self.bit_position & 7)) != 0
-
-    def read(self, into: bytearray, offset: int, count: int):
-        len_ = min(count, self.capacity - self.position - 1 + int(self.bit_aligned))
-        for idx in range(len_):
-            into[offset + idx] = self._read_byte()
-        return len_
-
-    def seek(self, offset: int, origin: int):
-        if origin == SEEK_SET:
-            self.bit_position = max(0, offset) << 3
-        elif origin == SEEK_CUR:
-            if offset > 0:
-                self.bit_position = max(self.bit_position + offset << 3, self.capacity << 3)
-            elif offset < 0:
-                self.bit_position = self.bit_position - (self.capacity - offset) << 3
-        elif origin == SEEK_END:
-            self.bit_position = max(0, self.capacity - offset) << 3
-        else:
-            raise ValueError(f"Invalid seek origin: {origin}")
-
-    def write(self, from_: bytearray, offset: int, count: int):
-        position = self.position
-        if self.bit_aligned:
-            if position + count >= self.capacity:
-                self._grow(count)
-            self._target[position:position + count] = from_[offset:offset + count]
-        else:
-            if position + count + 1 >= self.capacity:
-                self._grow(count)
-            for idx in range(count):
-                self._write_misaligned(from_[offset + idx])
-
-    def write_byte(self, value: int):
-        if value < 0 or value > 255:
-            raise ValueError("Value to write must be in range(0, 256)")
-        if self.bit_aligned:
-            if self.position + 1 >= self.capacity:
-                self._grow(1)
-            self._target[self.position] = value
-            self.position += 1
-        else:
-            if self.position + 2 >= self.capacity:
-                self._grow(1)
-            self._write_misaligned(value)
-        self._update_length()
-
     def _update_length(self):
         if self.bit_position > self._bit_length:
             self._bit_length = self.bit_position
@@ -188,12 +405,3 @@ class Buffer:
         self._target[p + 1] = (self._target[p + 1] & (255 << r)) | (value >> rc)
         self._target[p] = (self._target[p] & (255 >> rc)) | (value << r)
         self.bit_position += 8
-
-    def write_bit(self, bit: bool):
-        p = self.position
-        if self.bit_aligned and self.position == self.capacity:
-            self._grow(1)
-        r = self.bit_position & 7
-        self._target[p] = (self._target[p] & ~(1 << r)) | (int(bit) << r)
-        self.bit_position += 1
-        self._update_length()
